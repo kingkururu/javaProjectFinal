f651015fa2d036f77a8dde4b7f5ce07a2ea9427b
casvalrem123 <thsaud814@gmail.com>
2025-05-21 13:46:00 +0900

    text shows up only when run java not run

--- src/FirstJava/Ball.java
+++ src/FirstJava/Ball.java
@@ -1,26 +1,61 @@
+// import javax.swing.*;
+// import java.awt.*;
+// import java.awt.geom.Ellipse2D;
+
+// public class Ball extends Shape
+// {
+//     public Ball(int width, int height)
+//     {
+//         super(width, height);
+//         setPreferredSize(new Dimension(width, height));
+//         setOpaque(false);
+//     }
+
+//     public Ball(int width, int height, Point position, Color color, boolean moveState, int speed)
+//     {
+//         super(width, height, position, color, moveState, speed);
+//     }
+
+//    @Override
+//     protected void paintComponent(Graphics g) {
+//         super.paintComponent(g);
+//         Graphics2D g2d = (Graphics2D) g;
+//         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+//         g2d.setColor(getColor());
+//         int size = Math.min(getWidth(), getHeight());
+//         g2d.fillOval(0, 0, size, size);
+//     }
+// }
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
-
-public class Ball extends Shape
-{
-    public Ball(int width, int height)
-    {
+public class Ball extends Shape {
+    public Ball(int width, int height) {
         super(width, height);
-        setPreferredSize(new Dimension(width, height));
-        setOpaque(false);
+        initializeComponent(width, height);
     }
 
-    public Ball(int width, int height, Point position, Color color, boolean moveState, int speed)
-    {
+    public Ball(int width, int height, Point position, Color color, boolean moveState, int speed) {
         super(width, height, position, color, moveState, speed);
+        initializeComponent(width, height);
+    }
+
+    private void initializeComponent(int width, int height) {
+        setPreferredSize(new Dimension(width, height));
+        setOpaque(false); // Prevent background from being painted
     }
 
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor( super.getColor() );
-        g.fillOval( 0, 0, super.getWidth(), super.getHeight() );
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(getColor());
+        int size = Math.min(getWidth(), getHeight());
+        g2d.fillOval(0, 0, size, size);
     }
 }
